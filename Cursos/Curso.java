4a16d4813ee0ce1362de9c65f23e4a21d86c4f89
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-03-20 17:07:51 -0500

    Mejora rendimiento y lógica de métodos Se hace que los métodos de inserción impidan duplicados Se añade métodos para remover

--- Cursos/Curso.java
+++ Cursos/Curso.java
@@ -10,10 +10,10 @@ import Personas.Profesor;
 public class Curso {
     private String nombre;
     private int cantHoras;
-    private ArraySet<ArrayMap<Estudiante,Float>> estudiantes;
+    private ArraySet<ArrayMap<Estudiante, Float>> estudiantes;
     private Profesor profesor;
 
-    public Curso(String nombre, int cantHoras, Profesor profesor){
+    public Curso(String nombre, int cantHoras, Profesor profesor) {
         this.nombre = nombre;
         this.cantHoras = cantHoras;
         this.profesor = profesor;
@@ -21,9 +21,20 @@ public class Curso {
         this.estudiantes = new ArraySet<>();
     }
 
-    public boolean matricular(Estudiante est){
-        ArrayMap<Estudiante,Float> nuevo = new ArrayMap<>(est);
-        if (!this.estudiantes.contains(nuevo)) {
+    public boolean contains(Estudiante est) {
+        Iterator<ArrayMap<Estudiante, Float>> iterate = this.estudiantes.iterator();
+        while (iterate.hasNext()) {
+            ArrayMap<Estudiante, Float> element = iterate.next();
+            if (element != null && element.getElement().equals(est)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean matricular(Estudiante est) {
+        if (!this.contains(est)) {
+            ArrayMap<Estudiante, Float> nuevo = new ArrayMap<>(est);
             this.estudiantes.add(nuevo);
             est.addCurso(this);
             return true;
@@ -31,20 +42,32 @@ public class Curso {
         return false;
     }
 
-    public void setNota(Estudiante est, float nota){
-        Iterator<ArrayMap<Estudiante,Float>> students = this.estudiantes.iterator();
+    public boolean remover(Estudiante est) {
+        Iterator<ArrayMap<Estudiante, Float>> nuevo = this.estudiantes.iterator();
+        while (nuevo.hasNext()) {
+            ArrayMap<Estudiante, Float> std = nuevo.next();
+            if (std != null && std.getElement().equals(est)) {
+                est.removeCurso(this);
+                return this.estudiantes.remove(std);
+            }
+        }
+        return false;
+    }
+
+    public void setNota(Estudiante est, float nota) {
+        Iterator<ArrayMap<Estudiante, Float>> students = this.estudiantes.iterator();
         while (students.hasNext()) {
             ArrayMap<Estudiante, Float> student = students.next();
-            if (student!= null && student.getElement().equals(est)) {
+            if (student != null && student.getElement().equals(est)) {
                 student.add(nota);
             }
         }
     }
 
-    public Iterator<Float> getNotas(Estudiante est){
-        Iterator<ArrayMap<Estudiante,Float>> students = this.estudiantes.iterator();
+    public Iterator<Float> getNotas(Estudiante est) {
+        Iterator<ArrayMap<Estudiante, Float>> students = this.estudiantes.iterator();
         while (students.hasNext()) {
-            ArrayMap<Estudiante,Float> student = students.next();
+            ArrayMap<Estudiante, Float> student = students.next();
             if (student != null && student.getElement().equals(est)) {
                 return student.iterator();
             }
@@ -52,10 +75,10 @@ public class Curso {
         return null;
     }
 
-    public void imprimir(){
-        Iterator<ArrayMap<Estudiante,Float>> students = this.estudiantes.iterator();
+    public void imprimir() {
+        Iterator<ArrayMap<Estudiante, Float>> students = this.estudiantes.iterator();
         while (students.hasNext()) {
-            ArrayMap<Estudiante,Float> student = students.next();
+            ArrayMap<Estudiante, Float> student = students.next();
             if (student != null) {
                 System.out.println(student.getElement().toString());
             }
